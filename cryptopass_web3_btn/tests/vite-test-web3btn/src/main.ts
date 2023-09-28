dfb22c3bd6b274b47c5a5b584b755716f3b5a4b8
jimzord12 <w.tool@yahoo.gr>
2023-09-28 03:12:16 +0300

    Update main.ts

--- cryptopass_web3_btn/tests/vite-test-web3btn/src/main.ts
+++ cryptopass_web3_btn/tests/vite-test-web3btn/src/main.ts
@@ -38,7 +38,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <p> Do not forget to: </p>
 
     <ol style="text-align: left;">
-      <li> Start the WS Express Local Server</li>
+      <li> Start the WS Hono Local Server</li>
       <li> Make sure the WS's Wallet has ETH (see WS's console logs) </li>
       <li> Deploy the Contracts at Sepolia (if their Addresses are lost) </li>
       <li> From Remix IDE, use the WS's address to give Auth Privs to WS </li>
